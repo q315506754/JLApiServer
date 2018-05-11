2e21b0dca8b6c6477e87c05a6ef02e3eeddbc1ee
jiangli <jiangli@able-elec.com>
2018-05-11 18:55:39 +0800

    shell commit...

--- src/main/java/com/jiangli/api/controller/LoginController.kt
+++ src/main/java/com/jiangli/api/controller/LoginController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping("/user")
-class LoginController : BaseController() {
+ class LoginController : BaseController() {
     @Autowired
     var userService:UserService? = null
 
