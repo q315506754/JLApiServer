76a1b6ca412fe8267f93c3aed2f7c0aa85f40930
jiangli <jiangli@able-elec.com>
2019-12-09 18:01:43 +0800

    请求接口2

--- src/main/resources/static/js/page/zhichi_userinfo.js
+++ src/main/resources/static/js/page/zhichi_userinfo.js
@@ -1,14 +1,58 @@
+var api_prefix = "https://aries-app.g2s.cn";
+// var api_prefix = "http://120.92.138.210:20020";
+
+function pendRow(key,v) {
+    $(".container").append(`
+            <div class="row">   
+                <div class="title">${key}</div>
+                <div class="value">${v}</div>
+            </div>
+        `);
+}
+
+function requestAries(path,dt,cb) {
+    dt = dt || {};
+
+    $.ajax({
+        type: "POST",
+        url:api_prefix+"/getUserInfo",
+        beforeSend: function(request) {
+            // request.setRequestHeader("Referer","https://jl.g2s.cn");
+        },
+        data:dt,
+        success: function (response) {
+            console.log(response);
+
+            var dt = response.rt;
+
+            if (dt) {
+                cb(dt);
+            }
+        }
+    });
+}
+
 $(function () {
     console.log("loaded..");
 
     var obj = GetRequest();
-    for (var key in obj) {
+    var partnerId = obj['partnerId'];
 
-        $(".container").append(`
-            <div class="row">   
-                <label>${key}</label>
-                <div>${obj[key]}</div>
-            </div>
-        `);
-    }
+    // for (var key in obj) {
+    //     pendRow(key,obj[key]);
+    // }
+
+    pendRow('知室uid',partnerId);
+
+    requestAries("/getUserInfo",{
+        uid:partnerId
+    },function (dt) {
+        var user = dt.user;
+        pendRow('用户姓名',user.name);
+        pendRow('用户头像',`<img src="${user.avatar}"></img>`);
+        pendRow('手机号',user.mobile);
+        pendRow('邮箱',user.email);
+        pendRow('企业id',user.companyId);
+        pendRow('企业',user.describe);
+    })
 });
